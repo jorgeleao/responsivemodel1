86aea72cab26e7fdced137726c45bfd2ad5c99a1
JorgeLeao <jorge.leao@ufrj.br>
2023-02-24 03:41:19 -0300

    Advanced with footer menu

--- src/graphics.js
+++ src/graphics.js
@@ -2,19 +2,6 @@
 function Graphics() {
     return (
       <div>
-        <div style={{
-          border:"2px solid gray", borderRadius:"13px", backgroundColor:"white",
-          width:"128px",height:"38px"
-        }}>
-        <div style={{
-          position:"relative", top:"2px",left:"2px",
-          border:"2px solid gray", borderRadius:"10px", backgroundColor:"red",
-          width:"120px",height:"31px",lineHeight:"0px"
-        }}>
-          <span style={{color:"white",position:"relative",top:"14px",fontWeight:"bold"}}>SINÓTICO</span>
-        </div>
-
-        </div>
       </div>
     );
   }
--- src/menubar.js
+++ src/menubar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { useLocation } from "react-router-dom"
 import './menubar.css'
-import RedButton from './botao.vermelho.png'
+import MenuButton from './menubutton'
 
 
 export default function Menubar() {
@@ -11,54 +11,19 @@ export default function Menubar() {
         <div className="menubar">
             <div className="menuitems">
                 <div className="search-catalog">
-                
-
                     <Link to="/" className={location.pathname === "/" ?
                         "spacer menuitemselected" : "spacer menuitemNOTselected"}>
-                        <div style={{
-                        border:"2px solid gray", borderRadius:"13px", backgroundColor:"white",
-                        width:"128px",height:"38px"
-                        }}>
-                        <div style={{
-                        position:"relative", top:"2px",left:"2px",
-                        border:"2px solid gray", borderRadius:"10px", backgroundColor:"red",
-                        width:"120px",height:"31px",lineHeight:"0px"
-                        }}>
-                        <span style={{color:"white",position:"relative",top:"14px",fontWeight:"bold"}}>SINÓTICO</span>
-                        </div>
-                        </div>
+                            <MenuButton bgcolor={location.pathname === "/" ? "BLUE" : "gray"} titulo="SINÓTICO"/>
                     </Link>
 
                     <Link to="/grafico" className={location.pathname === "/grafico" ?
                                             "spacer menuitemselected" : "spacer menuitemNOTselected"}>
-                        <div style={{
-                        border:"2px solid gray", borderRadius:"13px", backgroundColor:"white",
-                        width:"128px",height:"38px"
-                        }}>
-                        <div style={{
-                        position:"relative", top:"2px",left:"2px",
-                        border:"2px solid gray", borderRadius:"10px", backgroundColor:"rgb(0,255,0)",
-                        width:"120px",height:"31px",lineHeight:"0px"
-                        }}>
-                        <span style={{color:"black",position:"relative",top:"14px",fontWeight:"bold"}}>GRÁFICOS</span>
-                        </div>
-                        </div>
+                            <MenuButton bgcolor={location.pathname === "/grafico" ? "green" : "gray"} titulo="GRÁFICO"/>
                     </Link>
 
                     <Link to="/events" className={location.pathname === "/events" ?
                                             "spacer menuitemselected" : "spacer menuitemNOTselected"}>
-                        <div style={{
-                        border:"2px solid gray", borderRadius:"13px", backgroundColor:"white",
-                        width:"128px",height:"38px"
-                        }}>
-                        <div style={{
-                        position:"relative", top:"2px",left:"2px",
-                        border:"2px solid gray", borderRadius:"10px", backgroundColor:"rgb(100,100,100,0.5)",
-                        width:"120px",height:"31px",lineHeight:"0px"
-                        }}>
-                        <span style={{color:"gray",position:"relative",top:"14px",fontWeight:"bold"}}>EVENTOS</span>
-                        </div>
-                        </div>
+                            <MenuButton bgcolor={location.pathname === "/events" ? "red" : "gray"} titulo="EVENTOS"/>
                     </Link>
                     
                 </div>
